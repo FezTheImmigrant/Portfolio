c4d366a3d5f65bdb7c8218b24b1e1643fa61821b
Sergio Orozco <sorozco0612@yahoo.com>
2020-03-18 20:25:20 -0600

    Setting Up React To communicate with Strapi

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
+
 import ReactDOM from "react-dom";
+
+import {ApolloProvider } from "react-apollo";
+
 import App from "./containers/App";
-import {BrowersRouter as Router} from "react-router-dom";
+
+import client from "./utils/apolloClient";
+
+//Router refers to how an app's end points (URI) respond to client requests
+import {BrowserRouter as Router} from "react-router-dom"; 
+
+import "./index.css";
 
 //Wrap app inside router
 ReactDOM.render(
     <Router>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </Router>,
     document.getElementById("root")
 )
