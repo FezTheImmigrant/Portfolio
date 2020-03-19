2ba8cf2ff1268ca3e6c8a997627b44a064873d86
Sergio Orozco <sorozco0612@yahoo.com>
2020-03-18 22:34:56 -0600

    Made navbar

---
+++ frontend/src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from "react";  
+import Query from "../Query";  
+import { Link } from "react-router-dom";
+
+const Nav = () => {  
+  return (
+    <div>
+     <nav className="uk-navbar-container" data-uk-navbar>
+        <div className="uk-navbar-left">
+          <ul className="uk-navbar-nav">
+            <li>
+              <a class="uk-text-bold" href="/">Machine Learning Engineer </a>
+            </li>
+          </ul>
+        </div>
+        <div className="uk-navbar-right">
+          <ul className="uk-navbar-nav">
+            <li><a class="uk-text-bold" href="article">Articles</a></li>
+            <li><a class="uk-text-bold" href="blog">Blog</a></li>
+            <li><a class="uk-text-bold" href="about">About</a></li>
+          </ul>
+        </div>
+      </nav> 
+    </div>
+  );
+};
+
+export default Nav;
