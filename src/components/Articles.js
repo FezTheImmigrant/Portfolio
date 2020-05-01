12c33c4f869b10dbad81efeae54436c5de4dc06b
Sergio Orozco <sorozco0612@yahoo.com>
2020-04-30 21:24:52 -0600

    bad change

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,13 +4,13 @@ export class Articles extends Component {
     render() {
         const articles = this.props.articles;
         return (
-            <div className="text-2xl black_transparent rounded-md  border border-white text-right fade-in-no-delay pt-2 w-full">
+            <div className="text-2xl bg-black rounded-md  border border-white text-right fade-in-no-delay pt-2 w-full">
                 <ul className=" h-40 overflow-hidden overflow-y-scroll mr-1" id="style-1">
                     {articles.map((article, i) => {
                         return (
                             <li className="mx-10 menu-pulse "id={i}>
                                 <a className="hover:no-underline" href={article.url}>
-                                    <p className="sm:text-gray sm:hover:text-white text-white">
+                                    <p className="text-gray hover:text-white">
                                         {article.title}
                                     </p>
                                 </a>
