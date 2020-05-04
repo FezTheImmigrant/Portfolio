8ba9a6ccd5c61721d9c54666f11bbebb5a4f4bb5
Sergio Orozco <sorozco0612@yahoo.com>
2020-05-03 18:52:50 -0600

    Updated Resume and made headshot less long on mobile

--- src/containers/BlogContainer.js
+++ src/containers/BlogContainer.js
@@ -12,9 +12,9 @@ export class BlogContainer extends Component {
             <div className="lg:pt-40 pt-32 min-h-screen">
                 <h1 className="text-white lg:text-6xl sm:text-4xl text-3xl text-center fade-in-no-delay"> Machine Learning Blog </h1>
                 <div className="text-gray-400 sm:text-xl text-xs text-center lg:mx-64 md:mx-32 sm:mx-32 mx-10  fade-in-no-delay mb-10">
-                    Welcome! This blog page is home to my thoughts on the world of Artificial Intelligence and Machine Learning.
+                    Welcome! This is where I discuss my thoughts on the world of Artificial Intelligence and Machine Learning.
                      I mainly write about my new discoveries on my journey to AI mastery, but so long as a topic pertains to AI/ML,
-                      it may find its way into my blog.
+                     it may find its way into my blog.
     
                 </div>
                 <div className="mx-auto">
